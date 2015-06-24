2d6c9e25842f1c5936b30b24aac681863219ed7a
welcomattic <mathieu.santostefano@gmail.com>
2015-06-24 11:49:27 +0200

    test(upload): update config structure

--- test/unit/tasks/s3/upload.js
+++ test/unit/tasks/s3/upload.js
@@ -18,15 +18,29 @@ describe('s3:upload task', function () {
     // Shipit config
     shipit.initConfig({
       test: {
-        awsConfig: {
-          "accessKeyId": "xxxxxxxxxxxxxxxxxxxx",
-          "secretAccessKey": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-          "Bucket": "mybucket",
-          "ACL": "public-read",
-          "region": "eu-west-1",
-          "syncedFolders": [
-            "api/web/media/**/*.*"
-          ]
+        aws: {
+          accessKeyId: 'xxxxxxxxxxxxxxxxxxxx',
+          secretAccessKey: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+          region: 'eu-west-1',
+          params: {
+            ACL: 'public-read',
+            Bucket: 'tstbucket',
+            StorageClass: 'REDUCED_REDUNDANCY'
+          },
+          syncParams: {
+            dirname: 'web',
+            options: {
+              base: '.',
+              whitelist: ['js', 'css', 'images'],
+              blacklist: [
+                '**/*',
+                '!**/*.md',
+                '!**/*.log',
+                '!**/*.coffee',
+                '!**/*.map'
+              ]
+            }
+          }
         }
       }
     });
@@ -36,10 +50,10 @@ describe('s3:upload task', function () {
   it('should have an awsConfig object, contains Bucket and good-size accessKeyId and secretAccessKey', function (done) {
     shipit.start('s3:upload', function (err) {
       if (err) return done(err);
-      expect(shipit.config.awsConfig).to.be.an('object');
-      expect(shipit.config.awsConfig.accessKeyId).to.have.length(20);
-      expect(shipit.config.awsConfig.secretAccessKey).to.have.length(40);
-      expect(shipit.config.awsConfig.Bucket).to.exist();
+      expect(shipit.config.aws).to.be.an('object');
+      expect(shipit.config.aws.accessKeyId).to.have.length(20);
+      expect(shipit.config.aws.secretAccessKey).to.have.length(40);
+      expect(shipit.config.aws.Bucket).to.exist();
       done();
     });
   });
