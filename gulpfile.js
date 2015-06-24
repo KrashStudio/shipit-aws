3944ad2cb6640f3de1b1142af15bd6768985f542
welcomattic <mathieu.santostefano@gmail.com>
2015-06-24 15:12:25 +0200

    chore(gulp): add gulpfile and a task to bump the versions of the project

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var replace = require('gulp-replace');
+var info = require('./package.json');
+var args = require('yargs').argv;
+var exec = require('child_process').exec;
+
+// $ gulp bump -v x.x.x
+gulp.task('bump', function () {
+  gulp.src([
+    'package.json'
+  ])
+  .pipe(replace(info.version, args.v || args.version))
+  .pipe(gulp.dest(function(file) {
+    return file.base;
+  }));
+  var child = exec('git tag ' + args.v || args.version, {}, function() {});
+  child.stdout.pipe(process.stdout);
+  child.stderr.pipe(process.stdout);
+});
